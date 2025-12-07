0c51926f2f2650ba5c8784a4904fdbcbe3879fd1
henrique <henriqueventurini.xp@gmail.com>
2025-12-06 23:38:45 -0300

    finalização do exercicio onze

--- src/exercicioOnze.java
+++ src/exercicioOnze.java
@@ -1,4 +1,25 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class exercicioOnze {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int[][] matriz = new int[3][3];
+
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz.length; j++) {
+                System.out.println("Digite um valor para a posição " + matriz[i][j]);
+                matriz[i][j] = sc.nextInt();
+            }
+        }
+
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[i].length; j++) {
+                System.out.print(matriz[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+    }
 }
